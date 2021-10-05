82a945c8ade779c299a7fe0583c357dbbae4a6b2
Hritik Pawar <73632558+unlikelycreator@users.noreply.github.com>
2021-10-05 12:42:20 +0530

    Build file uploaded

---
+++ build/js/adscript1.js
@@ -0,0 +1,8 @@
+atOptions = {
+    'key' : '74ae0cb3db034ecf529c35fca4e8e110',
+    'format' : 'iframe',
+    'height' : 50,
+    'width' : 320,
+    'params' : {}
+};
+document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.highperformancedformats.com/74ae0cb3db034ecf529c35fca4e8e110/invoke.js"></scr' + 'ipt>');
